3ec66176284a6575e21bb51ccf8f8d7b45e7a804
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-04-15 17:01:03 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/11399. ATM/ATM.cc
+++ C++17/백준/Silver/11399. ATM/ATM.cc
@@ -5,23 +5,30 @@
 using namespace std;
 
 int main(void) {
-	int person, time, sum_Alltime = 0;
-	vector<int> time_arr;
-	cin >> person;
+	int test;
+	cin >> test;
 
-	for (int i = 0; i < person; i++) {
-		cin >> time;
-		time_arr.push_back(time);
+	vector<int> vec(test);
+	vector<int> sum(test);
+
+	for (int i = 0; i < test; i++) {
+		cin >> vec[i];
 	}
 
-	sort(time_arr.begin(), time_arr.end());
-	
-	for (int i = 0; i < time_arr.size(); i++) {
-		int sum = 0;
-		for (int j = 0; j <= i; j++) {
-			sum += time_arr[j];
-		}
-		sum_Alltime += sum;
+	sort(vec.begin(), vec.end());
+
+	int sumeach = 0;
+
+	for (int i = 0; i < test; i++) {
+		sumeach += vec[i];
+		sum[i] = sumeach;
 	}
-	cout << sum_Alltime;
+
+	int ans = 0;
+	for (int i = 0; i < test; i++) {
+		ans += sum[i];
+	}
+
+	cout << ans;
+	return 0;
 }
