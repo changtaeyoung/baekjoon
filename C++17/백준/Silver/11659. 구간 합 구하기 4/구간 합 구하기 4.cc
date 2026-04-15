be19b7f1a553b8cc649ce229e8f58bdf6e791f87
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-04-15 16:55:07 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2412 KB -BaekjoonHub

--- C++17/백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ C++17/백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,24 +1,13 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int number_cnt, sum_cnt, v1, v2, number;
-	cin >> number_cnt >> sum_cnt;
-
-	vector<int> numbers_arr(number_cnt + 1);
-	numbers_arr[0] = 0;
-	for (int i = 1; i <= number_cnt; i++) {
-		cin >> number;
-		numbers_arr[i] = numbers_arr[i - 1] + number;
-	}
-
-	for (int i = 0; i < sum_cnt; i++) {
-		cin >> v1 >> v2;
-		cout << numbers_arr[v2] - numbers_arr[v1 - 1] << '\n';
-	}
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+<hr><center>nginx/1.22.1</center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
