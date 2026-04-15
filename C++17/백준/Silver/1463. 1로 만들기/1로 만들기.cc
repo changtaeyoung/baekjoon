8966531bfec6dbb37eb856ef94251fafa4053fb8
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-04-15 16:59:35 +0900

    [Silver III] Title: 1로 만들기, Time: 28 ms, Memory: 40964 KB -BaekjoonHub

--- C++17/백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ C++17/백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,24 +1,28 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int main(void) {
-	int number, cnt[1000001] = { 0,0,1,1, };
-	cin >> number;
+int main() {
+	int n;
+	cin >> n;
+	int dp[10000001] = { 0, };
 
-	for (int i = 4; i <= number; i++) {
-		if (i % 3 != 0 && i % 2 != 0) {
-			cnt[i] = cnt[i - 1] + 1;
-		}
-		else if (i % 2 == 0 && i % 3 == 0) {
-			cnt[i] = min(cnt[i / 2] + 1, cnt[i / 3] + 1);
-		}
-		else if (i % 3 == 0) {
-			cnt[i] = min(cnt[i / 3] + 1, cnt[i - 1] + 1);
-		}
-		else if (i % 2 == 0) {
-			cnt[i] = min(cnt[i / 2] + 1, cnt[i - 1] + 1);
-		}
-	}
-	cout << cnt[number];
+	dp[0] = 0;
+	dp[1] = 0;
+	dp[2] = 1;
+	dp[3] = 1;
+
+    for (int i = 4; i <= n; i++) {
+        if (i % 2 != 0 && i % 3 != 0)
+            dp[i] = dp[i - 1] + 1;
+        else if (i % 2 == 0 && i % 3 == 0)
+            dp[i] = min(dp[i / 2] + 1, dp[i / 3] + 1);
+        else if (i % 2 == 0)
+            dp[i] = min(dp[i / 2] + 1, dp[i - 1] + 1);
+        else if (i % 3 == 0)
+            dp[i] = min(dp[i / 3] + 1, dp[i - 1] + 1);
+    }
+	cout << dp[n];
+	return 0;
 }
