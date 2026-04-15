49eb91e02069c94220d9bd81002f908830ca0206
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-04-15 17:55:18 +0900

    [Silver IV] Title: 수 찾기, Time: 68 ms, Memory: 2412 KB -BaekjoonHub

--- C++17/백준/Silver/1920. 수 찾기/수 찾기.cc
+++ C++17/백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,35 +1,55 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+ 
 using namespace std;
+ 
+int arr[100001];
+ 
+void Solution(int n, int key){
+ 
+    int start = 0;
+    int end = n-1;
+    int mid;
+ 
+    while(end - start >= 0){
+        mid = (start + end)/2;
+ 
+        if(arr[mid] == key){  
+            printf("1\n");
+            return ;
+ 
+        }else if(arr[mid] > key) { 
+            end = mid - 1;
+ 
+        }else{  
+            start = mid + 1;
+        }
+    }
+ 
+    printf("0\n");
+    return ;
+}
+ 
+int main(void){
+ 
+    int n, m, tmp;
+ 
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+    scanf("%d", &n);
+ 
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    sort(arr, arr+n);   
+ 
 
-	int n, m, num;
-	vector<int> n_arr, m_arr;
-	cin >> n;
-
-	for (int i = 0; i < n; i++) {
-		cin >> num;
-		n_arr.push_back(num);
-	}
-	sort(n_arr.begin(), n_arr.end());
-
-	cin >> m;
-	for (int i = 0; i < m; i++) {
-		bool inArr = false;
-		cin >> num;
-		if (binary_search(n_arr.begin(), n_arr.end(), num)) {
-			inArr = true;
-		}
-		if (inArr) {
-			cout << 1 << '\n';
-		}
-		else {
-			cout << 0 << '\n';
-		}
-	}
+    scanf("%d", &m);
+ 
+    for(int i=0; i<m; i++){
+        scanf("%d", &tmp);
+        Solution(n, tmp);
+    }
+ 
+    return 0;
 }
