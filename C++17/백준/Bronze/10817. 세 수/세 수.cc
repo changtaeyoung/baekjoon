5c36c4f05da05dfc59ddca26998f3160db6752d0
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-04-15 18:06:20 +0900

    [Bronze IV] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a >= b && b >= c) {
+		cout << b << endl;
+	}
+	else if (b >= a && a >= c) {
+		cout << a << endl;
+	}
+	else if (a >= c && c >= b) {
+		cout << c << endl;
+	}
+	else if (c >= b && b >= a) {
+		cout << b << endl;
+	}
+	else if (c >= a && a >= b) {
+		cout << a << endl;
+	}
+	else if (b >= c && c >= a) {
+		cout << c << endl;
+	}
+	
+	return 0;
+}
