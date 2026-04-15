3919174f9c617449a9fe16ca81cb6b2cab81567f
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-04-15 17:44:56 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ C++17/백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,34 +1,29 @@
 #include <iostream>
-#include <vector>
+#include <string>
 #include <algorithm>
 
 using namespace std;
 
-int main(void) {
-	int n, m, card, max = -1;
-	vector<int> card_arr;
-	cin >> n >> m;
-
-	for (int i = 0; i < n; i++) {
-		cin >> card;
-		card_arr.push_back(card);
-	}
-
-	for (int i = 0; i < n - 2; i++) {
-		for (int j = i + 1; j < n - 1; j++) {
-			for (int k = j + 1; k < n; k++) {
-				if (card_arr[i] + card_arr[j] + card_arr[k] == m) {
-					cout << m;
-					return 0;
-				}
-				else {
-					if (card_arr[i] + card_arr[j] + card_arr[k] > max && card_arr[i] + card_arr[j] + card_arr[k] <= m) {
-						max = card_arr[i] + card_arr[j] + card_arr[k];
-					}
-				}
-			}
-		}
-	}
-	cout << max;
-	return 0;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int card[100] = { 0, };
+    for (int i = 0; i < n; i++) {
+        cin >> card[i];
+    }
+    int sum = 0, min=500000;
+    int goal;
+    for (int i = 0; i < n - 2; i++) {
+        for (int j = i + 1; j < n - 1; j++) {
+            for (int k = j + 1; k < n; k++) {
+                sum = card[i] + card[j] + card[k];
+                if (sum <= m && m - sum < min) {
+                    min = m - sum;
+                    goal = sum;
+                }
+            }
+        }
+    }
+    cout << goal;
+    return 0;
 }
