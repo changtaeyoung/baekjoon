626e0304aad2e6c043d49a99cd90002ca79e72bf
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-04-15 18:15:16 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int k = 0, q = 0, l = 0, v = 0, n = 0, p = 0;
+	cin >> k >> q >> l >> v >> n >> p;
+	cout << 1 - k << " " << 1 - q << " " << 2 - l << " " << 2 - v << " " << 2 - n << " " << 8 - p << endl;
+	return 0;
+}
