43a40d739dae34155e4391f74a3f8c8e32105922
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-04-15 18:05:44 +0900

    [Bronze III] Title: ACM 호텔, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int t, h, w, n;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> h >> w >> n;
+		int h1, w1;
+		h1 = n % h;
+		w1 = n / h;
+		if (h1 > 0) {
+			w1++;
+		}
+		else
+			h1 = h;
+		cout << h1 * 100 + w1 << endl;
+	}
+	return 0;
+}
