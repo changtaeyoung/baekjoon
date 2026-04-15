b710592c84a4328e0c2e9391dd0f7734ea1fd8f6
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-04-15 17:54:14 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 116 ms, Memory: 3572 KB -BaekjoonHub

--- C++17/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ C++17/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -5,21 +5,16 @@
 using namespace std;
 
 int main(void) {
-	int test, x, y;
-	pair<int, int> co;
-	vector<pair<int, int>> co_arr;
-	cin >> test;
-
-	for (int i = 0; i < test; i++) {
+	int n, x, y;
+	cin >> n;
+	vector<pair<int, int>> v;
+	for (int i = 0; i < n; i++) {
 		cin >> x >> y;
-		co.first = x;
-		co.second = y;
-		co_arr.push_back(co);
+		v.push_back(make_pair(x, y));
 	}
-
-	sort(co_arr.begin(), co_arr.end());
-
-	for (int i = 0; i < co_arr.size(); i++) {
-		cout << co_arr[i].first << " " << co_arr[i].second << '\n';
+	sort(v.begin(), v.end());
+	for (int i = 0; i < n; i++) {
+		cout << v[i].first << " " << v[i].second << "\n";
 	}
+	return 0;
 }
