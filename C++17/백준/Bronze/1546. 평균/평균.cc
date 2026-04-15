7d99c5edba07029a747a4fcf95e495a77e9c4981
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-04-15 18:09:15 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/1546. 평균/평균.cc
+++ C++17/백준/Bronze/1546. 평균/평균.cc
@@ -1,13 +1,14 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main(void) {
-	int n, score, max = -1, sum = 0;
 
-
+	int n = 0;
 	cin >> n;
+	int score = 0;
+	float sum = 0;
+	int max = 0;
 	for (int i = 0; i < n; i++) {
 		cin >> score;
 		sum += score;
@@ -16,5 +17,5 @@ int main(void) {
 		}
 	}
 
-	cout << (double)100 * sum / max / n;
+	cout << 100.0 * sum / n / max << endl;
 }
