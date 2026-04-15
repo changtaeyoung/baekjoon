76350b315b0e73aca512ffdeb52676fd24f7bff8
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-04-15 18:12:08 +0900

    [Bronze V] Title: A+B - 7, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int n = 0;
+	cin >> n;
+	int a = 0, b = 0;
+	for (int i = 1; i <= n; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i<< ": " << a + b << endl;
+	}
+	return 0;
+}
