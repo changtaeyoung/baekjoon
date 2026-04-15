8c7f0e42cf01d9e137b2d9ca3ee89f570ae7b634
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-04-15 18:16:12 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const*argv[]) {
+	string name;
+	cin >> name;
+	cout << name << "?\?!" << endl;
+	return 0;
+}
