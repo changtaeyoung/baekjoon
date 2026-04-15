a08af59c94659bcabf1cd0049fcf380de53a3f99
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-04-15 18:13:17 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int n = 0;
+	cin >> n;
+	int a = 0, b = 0;
+	for (int i = 1; i <= n; i++) {
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+
+	return 0;
+}
